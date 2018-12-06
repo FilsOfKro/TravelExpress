9af60c81215a904fd3ff00d9456c8227f246057a
Sébastien Palud <palud.sebastien@gmail.com>
2018-12-06 14:14:44 +0100

    projet spring

---
+++ TravelExpressBack/src/main/java/com/lemolas/TravelExpressBack/TravelExpressBackApplication.java
@@ -0,0 +1,12 @@
+package com.lemolas.TravelExpressBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelExpressBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelExpressBackApplication.class, args);
+	}
+}
