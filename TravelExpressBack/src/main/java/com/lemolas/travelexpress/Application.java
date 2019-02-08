7756269ac51f6130be0041a404b6b3a84360646e
Sébastien Palud <palud.sebastien@gmail.com>
2019-02-08 16:21:04 +0100

    more war stuff

--- TravelExpressBack/src/main/java/com/lemolas/travelexpress/Application.java
+++ TravelExpressBack/src/main/java/com/lemolas/travelexpress/Application.java
@@ -2,13 +2,23 @@ package com.lemolas.travelexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	/**
+	 * Used when run as WAR
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 }
