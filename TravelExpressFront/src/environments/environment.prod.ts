fe2a0478c87632e39eb8026527ec1d6bc47c2a6a
Sébastien Palud <palud.sebastien@gmail.com>
2020-06-15 23:39:26 +0200

    https

--- TravelExpressFront/src/environments/environment.prod.ts
+++ TravelExpressFront/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 
   // URL of production API
-  apiUrl: 'http://www.sebastien-palud.fr:8080/TravelExpressBack/'
+  apiUrl: 'https://www.sebastien-palud.fr/TravelExpressBack/'
 };
